6521f1f07dcaaba6b8499ff800ac69a3e4f34eea
DeFUCC <fond@fondfrukt.ru>
2021-02-16 20:15:34 +0300

    got timestamps

--- src/use/hashList.js
+++ src/use/hashList.js
@@ -3,19 +3,26 @@ import { gun } from 'store@gun-db'
 import { ref, reactive, watchEffect } from 'vue'
 import { mainRoom } from 'store@room'
 import { throttledWatch } from '@vueuse/core'
+import { getState } from '../store/gun-db'
 
 export function useHashList(tag = 'word', room = mainRoom.pub) {
   const obj = reactive({})
-
+  let timestamps = {}
   gun
     .get(`~${room}`)
     .get(`#${tag}`)
-    .map()
-    .on((data, key) => {
+    .on((d, k) => {
+      timestamps = d['_']['>']
+    })
+    .map((item, a, g) => {
+      return JSON.parse(item)
+    })
+    .on((data, key, g) => {
       let hash = key.slice(0, 44)
       let author = key.slice(-87)
-      let record = JSON.parse(data)
+      let record = data
       obj[hash] = obj[hash] || record
+      obj[hash].timestamp = timestamps[key]
       obj[hash].authors = obj[hash].authors || {}
       obj[hash].authors[author] = true
     })
